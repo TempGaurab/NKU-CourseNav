7f49ae542b727ef2a29b20c41f0b02743010696e
TempGaurab <gaurabusa@gmail.com>
2024-09-22 04:51:56 -0400

    initialized flask

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import os
-from flask import Flask
-from flask import Flask, render_template
-
+from flask import Flask,request, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/navigate', methods=['POST'])
+def navigate():
+    selected_course = request.form.get('selectedCourse')  # Get the input value
+    # Render a new template (course.html) with the selected course
+    return render_template('course.html', course=selected_course)
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug = True)
