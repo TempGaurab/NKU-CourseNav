33d86988a4f94b2fadf358898493c6c4d60f05e4
Sushant Shrestha <sushant.shrestha003@gmail.com>
2024-09-22 22:49:37 -0400

    to pull

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ def home():
 def navigate():
     selected_course = request.form.get('selectedCourse')  # Get the input value
     # Render a new template (course.html) with the selected course
+    
     return render_template('course.html', course=selected_course)
 
 if __name__ == '__main__':
